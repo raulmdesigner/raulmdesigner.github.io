25787752c2f15820f052db632479a389fe92e2f0
raul <137645018+raulmdesigner@users.noreply.github.com>
2026-09-15 13:17:13 -0300

    Update and rename supabase-config.js to index.html

--- iamurel/supabase-config.js
+++
@@ -1,4 +0,0 @@
-const IAMUREL_SUPABASE_URL = 'https://SEU_PROJETO.supabase.co';
-const IAMUREL_SUPABASE_ANON_KEY = 'SUA_CHAVE_PUBLICA_ANON';
-
-const iamurelSupabase = supabase.createClient(IAMUREL_SUPABASE_URL, IAMUREL_SUPABASE_ANON_KEY);
