ef442720801c124c9be85f489940db8b0009f96b
raul <137645018+raulmdesigner@users.noreply.github.com>
2026-09-17 10:41:35 -0300

    Delete iamurel directory

--- iamurel/assets/supabase-config.js
+++
@@ -1,4 +0,0 @@
-const IAMUREL_SUPABASE_URL = 'https://bbajcfnvcrgelaynyqdt.supabase.co';
-const IAMUREL_SUPABASE_ANON_KEY = 'sb_publishable_8ownlyYhKQf1x3bvfEa9jQ_La7_LZPP';
-
-const iamurelSupabase = supabase.createClient(IAMUREL_SUPABASE_URL, IAMUREL_SUPABASE_ANON_KEY);
